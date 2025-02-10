f2152584d5d4256e51c8e37373d7d1bfa301ef0d
Austin Abro <AustinAbro321@gmail.com>
2025-02-10 19:27:25 +0000

    dummy images and further test
    Signed-off-by: Austin Abro <AustinAbro321@gmail.com>

--- oras/main_test.go
+++ oras/main_test.go
@@ -7,13 +7,11 @@ import (
 	"time"
 )
 
-// Block all incoming network traffic
 func blockAllIncomingNetwork() error {
 	cmd := exec.Command("sudo", "iptables", "-I", "INPUT", "1", "-j", "DROP")
 	return cmd.Run()
 }
 
-// Unblock all incoming network traffic
 func unblockAllIncomingNetwork() error {
 	cmd := exec.Command("sudo", "iptables", "-D", "INPUT", "-j", "DROP")
 	return cmd.Run()
@@ -21,8 +19,7 @@ func unblockAllIncomingNetwork() error {
 
 // This test must be run as sudo
 func TestOrasNetworkInterruption(t *testing.T) {
-	t.SkipNow()
-	// Start a goroutine to block and then unblock the network
+	t.SkipNow() // comment line to run test
 	go func() {
 		time.Sleep(1 * time.Second)
 		fmt.Println("Blocking incoming network traffic...")
@@ -31,14 +28,13 @@ func TestOrasNetworkInterruption(t *testing.T) {
 			return
 		}
 
-		time.Sleep(5 * time.Second) // Keep the network blocked for 5 seconds
+		time.Sleep(5 * time.Second)
 		fmt.Println("Unblocking incoming network traffic...")
 		if err := unblockAllIncomingNetwork(); err != nil {
 			t.Errorf("failed to unblock incoming network: %v", err)
 		}
 	}()
 
-	// Start the ORAS pull operation
 	if err := doOras(); err != nil {
 		t.Fatalf("doOras failed: %v", err)
 	}
